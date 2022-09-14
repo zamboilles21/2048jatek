d6ba2dab4e7c8b56e1f135270c9d213c603a0361
zamboilles21 <zamboilles@turr.hu>
2022-09-14 08:17:52 +0200

    panelek

--- 2048/2048/Form1.Designer.cs
+++ 2048/2048/Form1.Designer.cs
@@ -29,22 +29,22 @@ namespace _2048
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.panel9 = new System.Windows.Forms.Panel();
-            this.panel10 = new System.Windows.Forms.Panel();
-            this.panel11 = new System.Windows.Forms.Panel();
-            this.panel12 = new System.Windows.Forms.Panel();
-            this.panel13 = new System.Windows.Forms.Panel();
-            this.panel14 = new System.Windows.Forms.Panel();
-            this.panel15 = new System.Windows.Forms.Panel();
-            this.panel16 = new System.Windows.Forms.Panel();
+            this.P1 = new System.Windows.Forms.Panel();
+            this.P3 = new System.Windows.Forms.Panel();
+            this.P2 = new System.Windows.Forms.Panel();
+            this.P4 = new System.Windows.Forms.Panel();
+            this.P8 = new System.Windows.Forms.Panel();
+            this.P7 = new System.Windows.Forms.Panel();
+            this.P6 = new System.Windows.Forms.Panel();
+            this.P5 = new System.Windows.Forms.Panel();
+            this.P12 = new System.Windows.Forms.Panel();
+            this.P11 = new System.Windows.Forms.Panel();
+            this.P10 = new System.Windows.Forms.Panel();
+            this.P9 = new System.Windows.Forms.Panel();
+            this.P16 = new System.Windows.Forms.Panel();
+            this.P15 = new System.Windows.Forms.Panel();
+            this.P14 = new System.Windows.Forms.Panel();
+            this.P13 = new System.Windows.Forms.Panel();
             this.Bbtn = new System.Windows.Forms.Button();
             this.WASD = new System.Windows.Forms.Button();
             this.Jbtn = new System.Windows.Forms.Button();
@@ -52,134 +52,134 @@ namespace _2048
             this.Fbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // P1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(10, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(86, 83);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.P1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P1.Location = new System.Drawing.Point(10, 12);
+            this.P1.Name = "P1";
+            this.P1.Size = new System.Drawing.Size(86, 83);
+            this.P1.TabIndex = 0;
+            this.P1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // panel2
+            // P3
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel2.Location = new System.Drawing.Point(194, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(86, 83);
-            this.panel2.TabIndex = 1;
+            this.P3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P3.Location = new System.Drawing.Point(194, 12);
+            this.P3.Name = "P3";
+            this.P3.Size = new System.Drawing.Size(86, 83);
+            this.P3.TabIndex = 1;
             // 
-            // panel3
+            // P2
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel3.Location = new System.Drawing.Point(102, 12);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(86, 83);
-            this.panel3.TabIndex = 1;
+            this.P2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P2.Location = new System.Drawing.Point(102, 12);
+            this.P2.Name = "P2";
+            this.P2.Size = new System.Drawing.Size(86, 83);
+            this.P2.TabIndex = 1;
             // 
-            // panel4
+            // P4
             // 
-            this.panel4.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel4.Location = new System.Drawing.Point(286, 12);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(86, 83);
-            this.panel4.TabIndex = 2;
+            this.P4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P4.Location = new System.Drawing.Point(286, 12);
+            this.P4.Name = "P4";
+            this.P4.Size = new System.Drawing.Size(86, 83);
+            this.P4.TabIndex = 2;
             // 
-            // panel5
+            // P8
             // 
-            this.panel5.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel5.Location = new System.Drawing.Point(286, 101);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(86, 83);
-            this.panel5.TabIndex = 6;
+            this.P8.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P8.Location = new System.Drawing.Point(286, 101);
+            this.P8.Name = "P8";
+            this.P8.Size = new System.Drawing.Size(86, 83);
+            this.P8.TabIndex = 6;
             // 
-            // panel6
+            // P7
             // 
-            this.panel6.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel6.Location = new System.Drawing.Point(194, 101);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(86, 83);
-            this.panel6.TabIndex = 4;
+            this.P7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P7.Location = new System.Drawing.Point(194, 101);
+            this.P7.Name = "P7";
+            this.P7.Size = new System.Drawing.Size(86, 83);
+            this.P7.TabIndex = 4;
             // 
-            // panel7
+            // P6
             // 
-            this.panel7.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel7.Location = new System.Drawing.Point(102, 101);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(86, 83);
-            this.panel7.TabIndex = 5;
+            this.P6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P6.Location = new System.Drawing.Point(102, 101);
+            this.P6.Name = "P6";
+            this.P6.Size = new System.Drawing.Size(86, 83);
+            this.P6.TabIndex = 5;
             // 
-            // panel8
+            // P5
             // 
-            this.panel8.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel8.Location = new System.Drawing.Point(10, 101);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(86, 83);
-            this.panel8.TabIndex = 3;
+            this.P5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P5.Location = new System.Drawing.Point(10, 101);
+            this.P5.Name = "P5";
+            this.P5.Size = new System.Drawing.Size(86, 83);
+            this.P5.TabIndex = 3;
             // 
-            // panel9
+            // P12
             // 
-            this.panel9.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel9.Location = new System.Drawing.Point(286, 190);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(86, 83);
-            this.panel9.TabIndex = 6;
+            this.P12.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P12.Location = new System.Drawing.Point(286, 190);
+            this.P12.Name = "P12";
+            this.P12.Size = new System.Drawing.Size(86, 83);
+            this.P12.TabIndex = 6;
             // 
-            // panel10
+            // P11
             // 
-            this.panel10.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel10.Location = new System.Drawing.Point(194, 190);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(86, 83);
-            this.panel10.TabIndex = 4;
+            this.P11.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P11.Location = new System.Drawing.Point(194, 190);
+            this.P11.Name = "P11";
+            this.P11.Size = new System.Drawing.Size(86, 83);
+            this.P11.TabIndex = 4;
             // 
-            // panel11
+            // P10
             // 
-            this.panel11.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel11.Location = new System.Drawing.Point(102, 190);
-            this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(86, 83);
-            this.panel11.TabIndex = 5;
+            this.P10.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P10.Location = new System.Drawing.Point(102, 190);
+            this.P10.Name = "P10";
+            this.P10.Size = new System.Drawing.Size(86, 83);
+            this.P10.TabIndex = 5;
             // 
-            // panel12
+            // P9
             // 
-            this.panel12.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel12.Location = new System.Drawing.Point(10, 190);
-            this.panel12.Name = "panel12";
-            this.panel12.Size = new System.Drawing.Size(86, 83);
-            this.panel12.TabIndex = 3;
+            this.P9.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P9.Location = new System.Drawing.Point(10, 190);
+            this.P9.Name = "P9";
+            this.P9.Size = new System.Drawing.Size(86, 83);
+            this.P9.TabIndex = 3;
             // 
-            // panel13
+            // P16
             // 
-            this.panel13.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel13.Location = new System.Drawing.Point(286, 279);
-            this.panel13.Name = "panel13";
-            this.panel13.Size = new System.Drawing.Size(86, 83);
-            this.panel13.TabIndex = 10;
+            this.P16.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P16.Location = new System.Drawing.Point(286, 279);
+            this.P16.Name = "P16";
+            this.P16.Size = new System.Drawing.Size(86, 83);
+            this.P16.TabIndex = 10;
             // 
-            // panel14
+            // P15
             // 
-            this.panel14.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel14.Location = new System.Drawing.Point(194, 279);
-            this.panel14.Name = "panel14";
-            this.panel14.Size = new System.Drawing.Size(86, 83);
-            this.panel14.TabIndex = 8;
+            this.P15.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P15.Location = new System.Drawing.Point(194, 279);
+            this.P15.Name = "P15";
+            this.P15.Size = new System.Drawing.Size(86, 83);
+            this.P15.TabIndex = 8;
             // 
-            // panel15
+            // P14
             // 
-            this.panel15.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel15.Location = new System.Drawing.Point(102, 279);
-            this.panel15.Name = "panel15";
-            this.panel15.Size = new System.Drawing.Size(86, 83);
-            this.panel15.TabIndex = 9;
+            this.P14.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P14.Location = new System.Drawing.Point(102, 279);
+            this.P14.Name = "P14";
+            this.P14.Size = new System.Drawing.Size(86, 83);
+            this.P14.TabIndex = 9;
             // 
-            // panel16
+            // P13
             // 
-            this.panel16.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel16.Location = new System.Drawing.Point(10, 279);
-            this.panel16.Name = "panel16";
-            this.panel16.Size = new System.Drawing.Size(86, 83);
-            this.panel16.TabIndex = 7;
+            this.P13.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.P13.Location = new System.Drawing.Point(10, 279);
+            this.P13.Name = "P13";
+            this.P13.Size = new System.Drawing.Size(86, 83);
+            this.P13.TabIndex = 7;
             // 
             // Bbtn
             // 
@@ -236,22 +236,22 @@ namespace _2048
             this.Controls.Add(this.Jbtn);
             this.Controls.Add(this.WASD);
             this.Controls.Add(this.Bbtn);
-            this.Controls.Add(this.panel13);
-            this.Controls.Add(this.panel14);
-            this.Controls.Add(this.panel15);
-            this.Controls.Add(this.panel16);
-            this.Controls.Add(this.panel9);
-            this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel10);
-            this.Controls.Add(this.panel11);
-            this.Controls.Add(this.panel6);
-            this.Controls.Add(this.panel12);
-            this.Controls.Add(this.panel7);
-            this.Controls.Add(this.panel8);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.P16);
+            this.Controls.Add(this.P15);
+            this.Controls.Add(this.P14);
+            this.Controls.Add(this.P13);
+            this.Controls.Add(this.P12);
+            this.Controls.Add(this.P8);
+            this.Controls.Add(this.P11);
+            this.Controls.Add(this.P10);
+            this.Controls.Add(this.P7);
+            this.Controls.Add(this.P9);
+            this.Controls.Add(this.P6);
+            this.Controls.Add(this.P5);
+            this.Controls.Add(this.P4);
+            this.Controls.Add(this.P3);
+            this.Controls.Add(this.P2);
+            this.Controls.Add(this.P1);
             this.Name = "A2048";
             this.Text = "2048";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -261,22 +261,22 @@ namespace _2048
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Panel panel9;
-        private System.Windows.Forms.Panel panel10;
-        private System.Windows.Forms.Panel panel11;
-        private System.Windows.Forms.Panel panel12;
-        private System.Windows.Forms.Panel panel13;
-        private System.Windows.Forms.Panel panel14;
-        private System.Windows.Forms.Panel panel15;
-        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel P1;
+        private System.Windows.Forms.Panel P3;
+        private System.Windows.Forms.Panel P2;
+        private System.Windows.Forms.Panel P4;
+        private System.Windows.Forms.Panel P8;
+        private System.Windows.Forms.Panel P7;
+        private System.Windows.Forms.Panel P6;
+        private System.Windows.Forms.Panel P5;
+        private System.Windows.Forms.Panel P12;
+        private System.Windows.Forms.Panel P11;
+        private System.Windows.Forms.Panel P10;
+        private System.Windows.Forms.Panel P9;
+        private System.Windows.Forms.Panel P16;
+        private System.Windows.Forms.Panel P15;
+        private System.Windows.Forms.Panel P14;
+        private System.Windows.Forms.Panel P13;
         private System.Windows.Forms.Button Bbtn;
         private System.Windows.Forms.Button WASD;
         private System.Windows.Forms.Button Jbtn;
